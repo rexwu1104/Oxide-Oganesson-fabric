29fdffb32b1576d2a3a565cfdc75941d290a1f25
rexwu1104 <rexwu9204@gmail.com>
2021-06-18 18:30:54 +0800

    second commit

---
+++ remappedSrc/com/real/world/mod/Mod.java
@@ -0,0 +1,13 @@
+package com.real.world.mod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Mod implements ModInitializer {
+
+    public static final String Mod_ID = "oxide-oganesson";
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
